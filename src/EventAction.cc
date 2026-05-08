ed2931560c7f8fca9ad638100c2eb67522854432
gulbazsher123-dot <gulbazsher123@gmail.com>
2026-05-08 11:09:27 +0500

    Add EventAction implementation

---
+++ src/EventAction.cc
@@ -0,0 +1,34 @@
+#include "EventAction.hh"
+#include "RunAction.hh"
+
+#include "G4Event.hh"
+#include "G4RunManager.hh"
+#include "g4root.hh"
+
+EventAction::EventAction(RunAction* runAction)
+: G4UserEventAction(),
+  fRunAction(runAction),
+  fEdep(0.)
+{
+}
+
+EventAction::~EventAction()
+{
+}
+
+void EventAction::BeginOfEventAction(const G4Event* event)
+{
+  fEdep = 0.;
+}
+
+void EventAction::EndOfEventAction(const G4Event* event)
+{
+  // accumulate statistics in run action
+  fRunAction->AddEdep(fEdep);
+
+  // get analysis manager
+  auto analysisManager = G4AnalysisManager::Instance();
+
+  // fill histograms
+  analysisManager->FillH1(0, fEdep);
+}
